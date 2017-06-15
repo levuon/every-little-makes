81114c365af7cf3ef867e749daa583adbbfcdcf3
lev <levuonliu@gmail.com>
2017-06-15 17:15:05 +0800

    index.js

--- src/dots/fp/base.js
+++ src/dots/fp/base.js
@@ -1,4 +1,6 @@
-
+var Either = require('ramda-fantasy').Either;
+var Left = Either.Left;
+var Right = Either.Right;
 
 // EXAMPLE:
 /**
@@ -75,3 +77,63 @@ function boot(user, defaultURL) {
 
 boot(joeUser, 'http://site.com/en'); //'http://site.com/sp'
 boot(undefined, 'http://site.com/en'); //'http://site.com/en'
+
+
+// ####
+const tax = R.curry((tax, price) => {
+  if (!_.isNumber(price)) return Left(new Error("Price must be numeric"));
+
+  return  Right(price + (tax * price));
+});
+
+const discount = R.curry((dis, price) => {
+  if (!_.isNumber(price)) return Left(new Error("Price must be numeric"));
+
+  if (price < 10) return Left(new Error("discount cant be applied for items priced below 10"));
+
+  return Right(price - (price * dis));
+});
+
+const addCaliTax = (tax(0.1));//10%
+
+const apply25PercDisc = (discount(0.25));// 25% discount
+
+const getItemPrice = (item) => Right(item.price);
+
+
+const displayTotal = (total) => { console.log('Total Price: ' + total) };
+
+const logError = (error) => { console.log('Error: ' + error.message); };
+
+const eitherLogOrShow = Either.either(logError, displayTotal);
+
+//api
+const showTotalPrice = (item) => eitherLogOrShow(getItemPrice(item).chain(apply25PercDisc).chain(addCaliTax));
+
+
+let tShirt = { name: 't-shirt', price: 11 };
+let pant = { name: 't-shirt', price: '10 dollars' }; //error
+let chips = { name: 't-shirt', price: 5 }; //less than 10 dollars error
+
+
+showTotalPrice(tShirt) // Total Is: 9.075
+showTotalPrice(pant)   // Error: Price must be numeric
+showTotalPrice(chips)  //Error: discount cant be applied for items priced below 10
+
+
+var fs = require('fs');
+// const R = require('ramda');
+var Task = require('data.task');
+//  readFile :: String -> Task(Error, JSON)
+var readFile = function(filename) {
+  return new Task(function(reject, result) {
+    fs.readFile(filename, 'utf-8', function(err, data) {
+      console.log('123');
+      err ? reject(err) : result(data);
+}); });
+};
+readFile("base.js")
+.fork(
+  err => console.log('err',err),
+  data => console.log('data',data)
+);
--- src/dots/fp/index.js
+++ src/dots/fp/index.js
@@ -1,9 +1,22 @@
 //
-require('./base');
+// require('./base');
+// require('./monad');
 
 
-require('./monad');
 
+//
+// const R = require('ramda');
+//
+//
+//
+// const add = R.curry(function(a, b){
+//   return a + b
+// })
+//
+// var list = [add(1), add(1), add(1)];
+// var sum = R.compose.apply(R, [add(1), add(1), add(1)])
+//
+// console.log( sum(1) );
 
 
 
