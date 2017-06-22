2dadeb180babc57d43fee49675f35088238e200d
lev <levuonliu@gmail.com>
2017-06-22 15:28:16 +0800

    css

--- src/dots/fp/index.js
+++ src/dots/fp/index.js
@@ -1,22 +1,8 @@
-//
-// require('./base');
+
+// require('./compose');
+// require('./tupperware');
 // require('./monad');
-
-
-
-//
-// const R = require('ramda');
-//
-//
-//
-// const add = R.curry(function(a, b){
-//   return a + b
-// })
-//
-// var list = [add(1), add(1), add(1)];
-// var sum = R.compose.apply(R, [add(1), add(1), add(1)])
-//
-// console.log( sum(1) );
+// require('./apFunctor');
 
 
 
